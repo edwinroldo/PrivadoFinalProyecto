dea669ebda277a19f41d9659e4cd07d1cb03f246
edwinroldo <edwinroldo@gmail.com>
2024-10-25 10:35:32 -0600

    Verificando Indes.js
    de carpeta principal por variable POR_SERV

--- src/index.js
+++ src/index.js
@@ -1,36 +1,33 @@
-//requerimos componentes a utilizar
+// Requerimos componentes a utilizar
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-//se importa taskRouter
-const taskRoutes = require('./routes/tasks.routes')
+// Se importa taskRouter
+const taskRoutes = require('./routes/tasks.routes');
 
-
-//se guarda en constante app
+// Se guarda en constante app
 const app = express();
-//comunica ambos servicios
+// Comunica ambos servicios
 app.use(cors());
-//ver en consola las peticiones
-app.use(morgan('dev'))
-// servidor express lee peticiones json
+// Ver en consola las peticiones
+app.use(morgan('dev'));
+// Servidor express lee peticiones JSON
 app.use(express.json());
 
+// Se utiliza taskRoutes
+app.use(taskRoutes);
 
-//Se utiliza taskRoutes
-app.use(taskRoutes)
-
-//Manejo de errores
+// Manejo de errores
 app.use((err, req, res, next) => {
     return res.json({
         message: err.message
-    })
-})
+    });
+});
 
-//asignamos puerto al servidor que funcionara en puerto 4000
-
+// Asignamos puerto al servidor que funcionará en el puerto 4000
 const PORT_SERV = process.env.PORT_SERV || 4000;
 
 app.listen(PORT_SERV, function() {
-    console.log('Servidor en puerto ', PORT_SERV)
-})
+    console.log('Servidor en puerto', PORT_SERV);
+});
